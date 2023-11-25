b8567c7683469f4ce8878fb48e78833e3da46a2a
yatluk-danyil <danyil.yatluk@student.tuke.sk>
2023-11-25 15:04:46 +0100

    add project

---
+++ src/test/java/com/ITZLO/parkingapp/ParkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITZLO.parkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
