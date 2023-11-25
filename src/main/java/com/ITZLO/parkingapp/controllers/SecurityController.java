5e2393efe87cf02ca88b881b0e906fece3d29a14
yatluk-danyil <danyil.yatluk@student.tuke.sk>
2023-11-25 16:39:47 +0100

    add security

--- src/main/java/com/ITZLO/parkingapp/controllers/SecurityController.java
+++ src/main/java/com/ITZLO/parkingapp/controllers/SecurityController.java
@@ -1,22 +1,34 @@
 package com.ITZLO.parkingapp.controllers;
 
 import com.ITZLO.parkingapp.models.RegistrationModel;
-import com.ITZLO.parkingapp.models.User;
 import com.ITZLO.parkingapp.services.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @AllArgsConstructor
 public class SecurityController {
 
     private final UserService userService;
 
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
     @PostMapping("/registration")
-    public ResponseEntity<User> registration(@RequestBody RegistrationModel registration){
-        return ResponseEntity.ok(userService.registerUserByRegistrationModel(registration));
+    public String registration(RegistrationModel registration){
+        userService.registerUserByRegistrationModel(registration);
+        return "index";
     }
 }
